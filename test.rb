af47d2d012c58d01a50845730d249f675383ed74
Tinzin <tiffany.tinzin@gmail.com>
2017-05-14 05:18:27 -0700

    template

---
+++ test.rb
@@ -0,0 +1,18 @@
+require 'gosu'
+
+class Game < Gosu::Window
+    def initialize
+        super 640, 480
+        self.caption = ""
+    end
+
+    def update
+    end
+
+    def draw
+    end
+
+    def 
+end
+
+Game.new.show
